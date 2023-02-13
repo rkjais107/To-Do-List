f42f5de31f0d3cdcf0d54a553db221ebdeb9a377
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-13 22:44:08 +0530

    code commit!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <main className="py-3">
         <Container>
           <Routes>
-            <Route />
+            <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
