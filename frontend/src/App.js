6f739b883f53902a020155c9a647f621ac64eaef
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-13 22:25:09 +0530

    code commit!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route />
+          </Routes>
+        </Container>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
