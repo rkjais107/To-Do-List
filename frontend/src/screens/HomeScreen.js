e2f06714404c750e6a5e1ea8bb90c47c03354477
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-14 22:14:17 +0530

    code commit!

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const HomeScreen = () => {
-  return <h1>HomeScreen</h1>;
+  const [comment, setComment] = useState("");
+  const postSubmitHandler = () => {
+    console.log("Comment Added!");
+  };
+  return (
+    <>
+      <h1>HomeScreen</h1>
+      <Container
+        style={{ backgroundColor: "blue", height: "500px", width: "100%" }}
+      >
+        <Row
+          style={{ backgroundColor: "violet", height: "15%", width: "100%" }}
+        ></Row>
+        <Row
+          style={{ backgroundColor: "green", height: "70%", width: "100%" }}
+        ></Row>
+        <Row style={{ backgroundColor: "red", height: "15%", width: "100%" }}>
+          <div style={{ marginTop: "12px" }}>
+            <Form onSubmit={postSubmitHandler}>
+              <input
+                type="text"
+                placeholder="Add a comment..."
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                style={{
+                  width: "80%",
+                  padding: "12px 20px",
+                  borderRadius: "10px",
+                  marginRight: "5px",
+                }}
+              />
+              {comment && (
+                <Button type="submit" variant="primary" className="form-margin">
+                  Post
+                </Button>
+              )}
+            </Form>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default HomeScreen;
