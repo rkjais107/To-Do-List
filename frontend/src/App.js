0db7f31779829a731e9b84666751a1f8c40e9441
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-14 22:53:15 +0530

    code commit!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import ListScreen from "./screens/ListScreen";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           </Routes>
         </Container>
       </main>
+      <Footer />
     </Router>
   );
 }
