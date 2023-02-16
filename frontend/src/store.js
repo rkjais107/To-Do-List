7ba9337f729a6f8dfd111c02e65c8727b78390bb
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-16 14:17:04 +0530

    code commit!

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import { allListsReducer } from "./reducers/listsReducers";
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  allLists: allListsReducer,
+});
 
 const initialState = {};
 const middleware = [thunk];
