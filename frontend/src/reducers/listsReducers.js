d4864ab3ea998b087e25517afa0a4f9cb473d70e
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-16 15:29:09 +0530

    code commit!

--- frontend/src/reducers/listsReducers.js
+++ frontend/src/reducers/listsReducers.js
@@ -2,6 +2,9 @@ import {
   ALL_LISTS_FAIL,
   ALL_LISTS_REQUEST,
   ALL_LISTS_SUCCESS,
+  INDIVIDUAL_LIST_FAIL,
+  INDIVIDUAL_LIST_REQUEST,
+  INDIVIDUAL_LIST_SUCCESS,
 } from "../constants/listsConstants";
 
 export const allListsReducer = (state = { lists: [] }, action) => {
@@ -11,6 +14,19 @@ export const allListsReducer = (state = { lists: [] }, action) => {
     case ALL_LISTS_SUCCESS:
       return { loading: false, lists: action.payload };
     case ALL_LISTS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const individualListReducer = (state = { list: {} }, action) => {
+  switch (action.type) {
+    case INDIVIDUAL_LIST_REQUEST:
+      return { ...state, loading: true };
+    case INDIVIDUAL_LIST_SUCCESS:
+      return { loading: false, list: action.payload };
+    case INDIVIDUAL_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
