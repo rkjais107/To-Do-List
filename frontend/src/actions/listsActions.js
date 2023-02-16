f8fd48edab86d0033b7fe4c27ae0365ce3c8b967
RAUNAK JAISWAL <rkjais107@gmail.com>
2023-02-16 14:32:41 +0530

    code commit!

---
+++ frontend/src/actions/listsActions.js
@@ -0,0 +1,29 @@
+import {
+  ALL_LISTS_FAIL,
+  ALL_LISTS_REQUEST,
+  ALL_LISTS_SUCCESS,
+} from "../constants/listsConstants";
+
+export const getAllLists = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ALL_LISTS_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.get("/api/list", config);
+    dispatch({
+      type: ALL_LISTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_LISTS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
